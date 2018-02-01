237e0eebc674bead671e303f98bca29104996602
shachi <bhavsarshachi@gmail.com>
2018-02-01 17:28:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,41 @@ import React, { Component } from 'react';
 import {JsonTree} from 'react-editable-json-tree'
 import data from './data.json'
 
+var jsonfile = require('jsonfile')
 require('./App.sass')
 
 class App extends Component {
+
+	  constructor(props) {
+        super(props);
+        this.state = {
+            data: props.data,
+        };
+        this.onFullyUpdate = this.onFullyUpdate.bind(this);
+    }
+    
+    onFullyUpdate(data) {
+        this.setState({
+            data,
+        });
+        let updatedData = JSON.stringify(data);
+        console.log(updatedData);
+        var file = 'tmp.json'
+        
+        jsonfile.writeFile(file, updatedData,function (err) {
+          console.error(err)
+        });
+    }
+
   render() {
+  
     return (
       <div>
         <header>
           <h1>Welcome to React</h1>
         </header>
         <div>
-          	<JsonTree data={data} />
+          	<JsonTree data={data}  onFullyUpdate={this.onFullyUpdate}/>
         </div>
       </div>
     );
