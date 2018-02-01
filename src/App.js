22b95f904fe5e403a8f59ebc3d3d9ddceb1e53d0
shachi <bhavsarshachi@gmail.com>
2018-02-01 15:03:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
+import {JsonTree} from 'react-editable-json-tree'
+import data from './data.json'
+
 require('./App.sass')
 
 class App extends Component {
   render() {
     return (
       <div>
-        <header className="App-header">
+        <header>
           <h1>Welcome to React</h1>
         </header>
-        <p>
-          Get started
-        </p>
+        <div>
+          	<JsonTree data={data} />
+        </div>
       </div>
     );
   }
