8a2e56283a84f7a95dd1972ecd60524e99c1ce79
shachi-bhavsar <bhavsarshachi@gmail.com>
2018-02-01 11:28:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+require('./App.sass')
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <h1>Welcome to React</h1>
+        </header>
+        <p>
+          Get started
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+//var path = require('path');
+var paths = require('path');
+module.exports = {
+
+    entry: './src/index.js',
+    output: {
+        path: paths.resolve(__dirname, ''),
+        filename: 'bundle.js',
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.(js|jsx)$/,
+                include: paths.resolve(__dirname, 'src'),
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.sass$/,
+                include: paths.resolve(__dirname, 'src'),
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    }
+};
