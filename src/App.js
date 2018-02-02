e6ae89777f1781e276c2b0ad6adcb1e7b3aa2c17
shachi <bhavsarshachi@gmail.com>
2018-02-02 15:48:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import {JsonTree} from 'react-editable-json-tree'
+import jsonData from './data.json'
 import data from './data.json'
-
+import JSONTree from 'react-json-tree'
 require('./App.sass')
-var jsonfile = require('jsonfile')
-const writeJsonFile = require('write-json-file');
+//var jsonfile = require('jsonfile')
+//const writeJsonFile = require('write-json-file');
 //var fs = require("fs");
+
 class App extends Component {
 
 	  constructor(props) {
@@ -22,25 +24,33 @@ class App extends Component {
         });
         let updatedData = JSON.stringify(data);
         console.log(updatedData);
-        var file = 'tmp.json'
+       /* var file = 'tmp.json'
        
         writeJsonFile(file, updatedData).then(() => {
           console.log('done');
         });
         jsonfile.writeFile(file, updatedData,function (err) {
           console.error(err)
-        });
+        });*/
     }
 
   render() {
-  
+    
     return (
       <div>
         <header>
           <h1>Welcome to React</h1>
         </header>
-        <div>
-          	<JsonTree data={data}  onFullyUpdate={this.onFullyUpdate}/>
+        <div style={{float:'left',width:"45%"}}>
+        Original Data:
+            <JSONTree data={jsonData} />
+        </div>
+        <div style={{float:'right',width:"50%"}}>
+        Updated Data:
+            <JsonTree data={data}  
+              onFullyUpdate={this.onFullyUpdate} 
+              readOnly={(name, value, keyPath) => (keyPath[keyPath.length - 1] === 'id')}
+            />
         </div>
       </div>
     );
